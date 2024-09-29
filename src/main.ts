a4d0a53919dcc1cef1bc7474f1dbbca9e0b40718
Dorian Czupryniak <dorian.czupryniak@gmail.com>
2024-09-29 22:31:46 +0200

    fix bug with cors

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// prettier-ignore
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -7,10 +8,9 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.enableCors({
     allowedHeaders: ['content-type'],
-    origin: process.env.FRONTEND_URL || [
-      'https://use-guitar-panel-nest.vercel.app',
-      'https://use-guitar-nest.vercel.app',
-    ],
+    origin: 
+    process.env.FRONTEND_URL ||
+    ['https://use-guitar-panel-nest.vercel.app','https://use-guitar-nest.vercel.app'],
     credentials: true,
   });
   await app.listen(3000);
