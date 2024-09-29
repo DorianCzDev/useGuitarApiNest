a0fd77506adb0e2e09ddb4f3d4f615a04d545ed7
Dorian Czupryniak <dorian.czupryniak@gmail.com>
2024-09-29 22:47:17 +0200

    fix bug with cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 // prettier-ignore
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{cors: true});
 
   app.setGlobalPrefix('api');
   app.enableCors({
